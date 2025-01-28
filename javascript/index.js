94b767c2ee207c1043c8b1639dc4657bffb5e4d0
AleksandraTronb <aleksandratronb@gmail.com>
2025-01-28 22:21:00 +0000

    Added styles with CSS

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,1 @@
-function ChangeName() {
-  let name = prompt("What is your name?");
-  console.log(name);
-  let changetitle = document.querySelector("#title-name");
-  changetitle.innerHTML = `Hello ${name}`;
-}
-let ChangeButton = document.querySelector("#button-name");
-ChangeButton.addEventListener("click", ChangeName);
+
