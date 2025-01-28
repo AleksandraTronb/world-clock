2334e571360508a29b0d6f73d1fc68552de6c015
AleksandraTronb <aleksandratronb@gmail.com>
2025-01-28 17:29:11 +0000

    add button for change name

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,8 @@
-alert("Hello world!");
+function ChangeName() {
+  let name = prompt("What is your name?");
+  console.log(name);
+  let changetitle = document.querySelector("#title-name");
+  changetitle.innerHTML = `Hello ${name}`;
+}
+let ChangeButton = document.querySelector("#button-name");
+ChangeButton.addEventListener("click", ChangeName);
