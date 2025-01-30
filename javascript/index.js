947142609ff43056731e131e7f26da9ab1670964
AleksandraTronb <aleksandratronb@gmail.com>
2025-01-30 18:53:52 +0000

    Added current date and time of a location.

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,24 @@
-let sydneyElement = document.querySelector("#sydney");
-let sydneyDateElement = sydneyElement.querySelector(".date");
-let sydneyTimeElement = sydneyElement.querySelector(".time");
-sydneyDateElement.innerHTML = "30th January 2025";
-sydneyTimeElement.innerHTML = "1:38:30 <small>AM</small>";
+function updateTime() {
+  let SydneyElement = document.querySelector("#sydney");
+  let SydneyDateElement = SydneyElement.querySelector(".date");
+  let SydneyTimeElement = SydneyElement.querySelector(".time");
+  let SydneyTime = moment().tz("Australia/Sydney");
+  SydneyDateElement.innerHTML = `${SydneyTime.format("LL")}`;
+  SydneyTimeElement.innerHTML = SydneyTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //[]- js misses everithing in square brackets
+  /*sydneyTimeElement.innerHTML = `${sydneyTime.format(
+  "h:mm:ss"
+)} <small>${sydneyTime.format("A")}</small>`;*/
+  let LondonElement = document.querySelector("#london");
+  let LondonDateElement = LondonElement.querySelector(".date");
+  let LondonTimeElement = LondonElement.querySelector(".time");
+  let LondonTime = moment().tz("Europe/London");
+  LondonDateElement.innerHTML = `${LondonTime.format("LL")}`;
+  LondonTimeElement.innerHTML = LondonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
