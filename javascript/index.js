4c188fc47ef2cb358fb6f3b3447b6f8d7c3017ec
AleksandraTronb <aleksandratronb@gmail.com>
2025-01-30 17:31:50 +0000

    js basic

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,5 @@
-
+let sydneyElement = document.querySelector("#sydney");
+let sydneyDateElement = sydneyElement.querySelector(".date");
+let sydneyTimeElement = sydneyElement.querySelector(".time");
+sydneyDateElement.innerHTML = "30th January 2025";
+sydneyTimeElement.innerHTML = "1:38:30 <small>AM</small>";
